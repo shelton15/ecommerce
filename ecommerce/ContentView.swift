749b4855e5587b469219d26667c12cbadb8ec493
shelton15 <88257757+shelton15@users.noreply.github.com>
2024-09-16 22:19:18 +0100

    first commit of the comparo UI test, bottom navigation complete

--- ecommerce/ContentView.swift
+++ ecommerce/ContentView.swift
@@ -9,80 +9,166 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @State var currentTab: Tab = .Home
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
+    @Namespace var animation
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+        TabView(selection: $currentTab) {
+            
+            Text("Home View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Home)
+            
+            Text("Search View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Search)
+            
+            Text("Notifications View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Notifications)
+            
+            Text("Cart View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Cart)
+            
+            Text("Profile View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Profile)
+            
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        .overlay(
+            HStack(spacing: 0) {
+                ForEach(Tab.allCases,  id: \.rawValue) {tab in
+                    TabButton(tab: tab)
+                    
+                }
+                .padding(.vertical)
+                .padding(.bottom, getSafeArea().bottom == 0 ? 5 : (getSafeArea().bottom - 15))
+                .background(Color("kSecondary"))
             }
-        }
+            ,
+            alignment: .bottom
+        )
+        .ignoresSafeArea(.all, edges: .bottom)
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    
+    func TabButton(tab: Tab) -> some View {
+        
+        GeometryReader {proxy in
+            Button(action: {
+                withAnimation(.spring()){
+                    currentTab = tab
+                }
+            },label: {
+                VStack(spacing: 0){
+                    Image(systemName: currentTab == tab ? tab.rawValue + ".fill": tab.rawValue)
+                        .resizable()
+                        .foregroundColor(Color("kPrimary"))
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 25, height: 25)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            ZStack{
+                                if currentTab == tab {
+                                    MaterialEffect(style: .light)
+                                        .clipShape(Circle())
+                                        .matchedGeometryEffect(id: "Tab", in: animation)
+                                    Text(tab.Tabname)
+                                        .foregroundColor(.primary)
+                                        .font(.footnote)
+                                        .padding(.top, 50)
+                                }
+                            }
+                        ).containerShape(Rectangle())
+                        .offset(y: currentTab == tab ? -15 : 0)
+                }
+            })
         }
+        .frame(height: 25)
+        
     }
+    
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
+}
+
+enum Tab: String, CaseIterable {
+    
+    case Home = "house"
+    case Search = "magnifyingglass.circle"
+    case Notifications = "bell"
+    case Cart = "bag"
+    case Profile = "person"
+    
+    var Tabname: String {
+        
+        switch self {
+            
+        case .Home:
+            return "Home"
+        case .Search:
+            return "Search"
+        case .Notifications:
+            return "Notifications"
+        case .Cart:
+            return "Cart"
+        case .Profile:
+            return "Profile"
+            
+        }
+        
+    }
+    
+}
+
+
+extension View {
+    
+    func getSafeArea() -> UIEdgeInsets {
+        
+        guard let screen = UIApplication.shared.connectedScenes.first as?
+                UIWindowScene else {
+            return .zero
+        }
+        
+        guard let safeArea = screen.windows.first?.safeAreaInsets else {
+            return .zero
+        }
+        
+        return safeArea
+    }
+    
+}
+
+struct MaterialEffect: UIViewRepresentable {
+    
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
+    }
+    
 }
